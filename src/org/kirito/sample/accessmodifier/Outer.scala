18d5870a92f6085999c72176046e6ade72137fb3
gitpangbo <mailpangbo@163.com>
2017-03-07 15:20:30 +0800

    pangbo push scala-sample

---
+++ src/org/kirito/sample/accessmodifier/Outer.scala
@@ -0,0 +1,21 @@
+package org.kirito.sample.accessmodifier
+
+/**
+  * Created by pangbo on 2017/2/23.
+  */
+class Outer {
+
+  class Inner {
+    private def f(): Unit = {
+      print("f")
+    }
+
+    class InnerMost {
+      f() // 正确
+    }
+
+  }
+
+  (new Inner).f() //错误
+
+}
---
+++ src/org/kirito/sample/accessmodifier/package.scala
@@ -0,0 +1,23 @@
+package org.kirito.sample.accessmodifier
+
+/**
+  * Created by pangbo on 2017/2/23.
+  */
+package p {
+
+  class Super {
+    protected def f(): Unit = {
+      print("f")
+    }
+  }
+
+  class Sub extends Super {
+    f()
+  }
+
+  class Other {
+    (new Super).f() //错误
+  }
+
+}
+
